196e0e5b987a6bb96d6eee418d6c021c6dbb10c8
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-06-02 18:48:25 +0100

    matlab: scripts for prediction Words occurrence

---
+++ src/matlab/predict_W_given_A_E_F.m
@@ -0,0 +1,24 @@
+% GLU 2017, p(W | A, E, F)
+% Giovanni Saponaro, Giampiero Salvi
+
+%% configure BNT and other paths
+addpath(genpath('~/matlab/toolbox/FullBNT-1.0.4'))
+addpath('.');
+
+% set to full path to <vislab svn repository>/app/baltazar/Affordances/speech/bayesian_net
+LanguageBoostrapping_root = ('~/NOBACKUP/vislab/app/baltazar/Affordances/speech/bayesian_net');
+
+addpath([LanguageBoostrapping_root '/matlab'])
+
+%% load Bayesian Network from .mat
+load('BN_lab.mat');
+
+%% go to directory with BN functions
+cd(LanguageBoostrapping_root);
+
+%% enter node evidence for Action, Effect and Features (object) nodes
+nodevaluepairs = {'Action', 'tap', 'ObjVel', 'fast', 'ObjHandVel', 'fast', 'Color', 'yellow', 'Shape', 'circle', 'Size', 'small'};
+netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
+
+%% extract predictions (posteriors) of Words being said
+wordProbsWithA = BNGetWordProbs(netobj_lab)
