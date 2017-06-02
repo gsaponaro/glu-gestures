dd13f1e8364f3222da3084313957da8d64e8961f
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-06-02 18:36:16 +0100

    matlab: use BN .mat; generalize posterior func

--- src/matlab/BNSoftPredictionAccuracy2.m
+++ src/matlab/BNSoftPredictionAccuracy2.m
@@ -1,31 +1,6 @@
-%function results = BNSoftPredictionAccuracy(netobj, questions, answers)
-function results = BNSoftPredictionAccuracy2(netobj)
+function marg = BNSoftPredictionAccuracy2(netobj, posteriornodes)
 
-%actionnode = BNWhichNode(netobj, 'Action');
-%colornode = BNWhichNode(netobj, 'Color');
-%shapenode = BNWhichNode(netobj, 'Shape');
-%sizenode = BNWhichNode(netobj, 'Size');
-%predictionNodes =  [actionnode colornode shapenode sizenode];
-objvelnode = BNWhichNode(netobj, 'ObjVel');
-objhandvelnode = BNWhichNode(netobj, 'ObjHandVel');
-predictionNodes = [objvelnode objhandvelnode];
+% from strings to integers
+predictionNodes = cellfun(@(x) BNWhichNode(netobj,x), posteriornodes);
 
-%for qnum = 1:Ntest
-    % enter evidence
-    %netobj = BNEnterWordEvidence(netobj, questions{qnum}, 0);
-    % get probabilities for affordance nodes
-    marg = marginal_nodes(netobj.engine, predictionNodes);
-%     % convert answers into multidimensional array
-%     ans_sizes = netobj.node_sizes(predictionNodes);
-%     ans_array = 1;
-%     first_idx = 0;
-%     for dim=1:length(ans_sizes) % [3 4 2 3]
-%         idxs = first_idx + (1:ans_sizes(dim));
-%         first_idx = first_idx + ans_sizes(dim); 
-%         ans_array = multikron(ans_array, answers(qnum,idxs));
-%     end
-%     % compare to answer
-%     scores = ans_array .* marg.T
-%     results(qnum) = sum(scores(:));
-%end
-results = marg;
+marg = marginal_nodes(netobj.engine, predictionNodes);
--- src/matlab/predict_E_given_A_F.m
+++ src/matlab/predict_E_given_A_F.m
@@ -9,35 +9,48 @@ addpath('.');
 LanguageBoostrapping_root = ('~/NOBACKUP/vislab/app/baltazar/Affordances/speech/bayesian_net');
 
 addpath([LanguageBoostrapping_root '/matlab'])
+
+%% load Bayesian Network from .mat
+load('BN_lab.mat');
+
+%% go to directory with BN functions
 cd(LanguageBoostrapping_root);
 
-%% 1) create initial word-affordance network
-netobj_lab = createBN('config/networkDefinition.txt');
+%% uncomment to re-train the network
+% %% 1) create initial word-affordance network
+% netobj_lab = createBN('config/networkDefinition.txt');
+% 
+% %% 2) set default properties (node types, initial connections...)
+% netobj_lab = BNSetDefaults(netobj_lab);
+% 
+% %% 3) load data from text file (coming from ../asr_htk/workdir)
+% netobj_lab = BNLoadData(netobj_lab, 'data/sent-1-5_lab_aff_bag.txt');
+% 
+% %% 3) learn the structure of the net
+% netobj_lab = BNLearnStructure(netobj_lab);
+% 
+% %% 4) learn the parameters of the net
+% netobj_lab = BNLearnParameters(netobj_lab);
 
-%% 2) set default properties (node types, initial connections...)
-netobj_lab = BNSetDefaults(netobj_lab);
-
-%% 3) load data from text file (coming from ../asr_htk/workdir)
-netobj_lab = BNLoadData(netobj_lab, 'data/sent-1-5_lab_aff_bag.txt');
-
-%% 3) learn the structure of the net
-netobj_lab = BNLearnStructure(netobj_lab);
-
-%% 4) learn the parameters of the net
-netobj_lab = BNLearnParameters(netobj_lab);
+%% save Bayesian Network to .mat
+%save('BN_lab.mat', 'netobj_lab');
 
 %% enter node evidence for Action and Features (object) nodes
 nodevaluepairs = {'Action', 'tap', 'Color', 'yellow', 'Shape', 'circle', 'Size', 'small'};
 netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
 
-%% extract predictions as in the first part of BNSoftPredictionAccuracy, now called BNSoftPredictionAccuracy2
-pred_lab_circle = BNSoftPredictionAccuracy2(netobj_lab);
+%% extract predictions (posteriors)
+posteriornodes = {'ObjVel', 'ObjHandVel'};
+pred_lab_circle = BNSoftPredictionAccuracy2(netobj_lab, posteriornodes);
+disp('p(ObjVel, ObjHandVel | A=tap, F=circle small)');
 pred_lab_circle.T
 
 %% enter node evidence for Action and Features (object) nodes
 nodevaluepairs = {'Action', 'tap', 'Color', 'blue', 'Shape', 'box', 'Size', 'big'};
+disp('p(ObjVel, ObjHandVel | A=tap, F=box big)');
 netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
 
-%% extract predictions as in the first part of BNSoftPredictionAccuracy, now called BNSoftPredictionAccuracy2
-pred_lab_box = BNSoftPredictionAccuracy2(netobj_lab);
+%% extract predictions (posteriors)
+posteriornodes = {'ObjVel', 'ObjHandVel'};
+pred_lab_box = BNSoftPredictionAccuracy2(netobj_lab, posteriornodes);
 pred_lab_box.T
