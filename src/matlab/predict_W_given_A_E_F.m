1297fbaa673a6fb8c329ad560b8c54198293d2f8
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-06-04 03:12:45 +0100

    matlab: comment old experiment with not sign. result

--- src/matlab/predict_W_given_A_E_F.m
+++ src/matlab/predict_W_given_A_E_F.m
@@ -16,21 +16,22 @@ load('BN_lab.mat');
 %% go to directory with BN functions
 cd(LanguageBoostrapping_root);
 
-%% enter node evidence for Action, Effect and Features (object) nodes
-nodevaluepairs = {'Action', 'tap', 'ObjVel', 'fast', 'ObjHandVel', 'fast', 'Color', 'yellow', 'Shape', 'circle', 'Size', 'small'};
-netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
-
-%% print Word node names
-wordNames = netobj_lab.nodeNames(1,9:end);
-
-%% extract predictions (posteriors) of Words being said
-wordProbsWithA = BNGetWordProbs(netobj_lab);
-
-%% print Word->prob table
-numWords = 49;
-resultWithA = cell(numWords,2);
-for (w = 1:numWords)
-    resultWithA{w,1} = string(wordNames(w));
-    resultWithA{w,2} = wordProbsWithA(1,w);
-end
-resultWithA
+%% old experiment
+% %% enter node evidence for Action, Effect and Features (object) nodes
+% nodevaluepairs = {'Action', 'tap', 'ObjVel', 'medium', 'ObjHandVel', 'fast', 'Color', 'yellow', 'Shape', 'circle', 'Size', 'small'};
+% netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
+% 
+% %% print Word node names
+% wordNames = netobj_lab.nodeNames(1,9:end);
+% 
+% %% extract predictions (posteriors) of Words being said
+% wordProbsWithA = BNGetWordProbs(netobj_lab);
+% 
+% %% print Word->prob table
+% numWords = 49;
+% resultWithA = cell(numWords,2);
+% for (w = 1:numWords)
+%     resultWithA{w,1} = string(wordNames(w));
+%     resultWithA{w,2} = wordProbsWithA(1,w);
+% end
+% resultWithA
--- src/matlab/predict_W_given_E_F.m
+++ src/matlab/predict_W_given_E_F.m
@@ -16,21 +16,22 @@ load('BN_lab.mat');
 %% go to directory with BN functions
 cd(LanguageBoostrapping_root);
 
-%% enter node evidence for Action, Effect and Features (object) nodes
-nodevaluepairs = {'ObjVel', 'fast', 'ObjHandVel', 'fast', 'Color', 'yellow', 'Shape', 'circle', 'Size', 'small'};
-netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
-
-%% print Word node names
-wordNames = netobj_lab.nodeNames(1,9:end);
-
-%% extract predictions (posteriors) of Words being said
-wordProbsWithoutA = BNGetWordProbs(netobj_lab);
-
-%% print Word->prob table
-numWords = 49;
-resultWithoutA = cell(numWords,2);
-for (w = 1:numWords)
-    resultWithoutA{w,1} = string(wordNames(w));
-    resultWithoutA{w,2} = wordProbsWithoutA(1,w);
-end
-resultWithoutA
+%% old experiment
+% %% enter node evidence for Action, Effect and Features (object) nodes
+% nodevaluepairs = {'ObjVel', 'fast', 'ObjHandVel', 'medium', 'Color', 'yellow', 'Shape', 'circle', 'Size', 'small'};
+% netobj_lab = BNEnterNodeEvidence(netobj_lab, nodevaluepairs, 0);
+% 
+% %% print Word node names
+% wordNames = netobj_lab.nodeNames(1,9:end);
+% 
+% %% extract predictions (posteriors) of Words being said
+% wordProbsWithoutA = BNGetWordProbs(netobj_lab);
+% 
+% %% print Word->prob table
+% numWords = 49;
+% resultWithoutA = cell(numWords,2);
+% for (w = 1:numWords)
+%     resultWithoutA{w,1} = string(wordNames(w));
+%     resultWithoutA{w,2} = wordProbsWithoutA(1,w);
+% end
+% resultWithoutA
